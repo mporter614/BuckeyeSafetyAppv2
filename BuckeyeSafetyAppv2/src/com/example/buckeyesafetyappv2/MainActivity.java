9614ee6f455810560e6139c117284d0eee6e4037
mporter614 <m.porter614@gmail.com>
2014-04-06 21:24:36 -0400

    Settings now save

--- BuckeyeSafetyAppv2/src/com/example/buckeyesafetyappv2/MainActivity.java
+++ BuckeyeSafetyAppv2/src/com/example/buckeyesafetyappv2/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 
@@ -13,14 +14,6 @@ public class MainActivity extends Activity implements OnClickListener {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);	
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
 	}
 	
 	public void onClick (View view) {
--- BuckeyeSafetyAppv2/src/com/example/buckeyesafetyappv2/SettingsActivity.java
+++ BuckeyeSafetyAppv2/src/com/example/buckeyesafetyappv2/SettingsActivity.java
@@ -2,45 +2,80 @@ package com.example.buckeyesafetyappv2;
 
 import android.support.v4.app.Fragment;
 import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.os.Build;
 
 public class SettingsActivity extends Activity {
 
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_settings);
+		
+		RadioGroup radioGroup = (RadioGroup) findViewById(R.id.radioGroup1);
 
-//		if (savedInstanceState == null) {
-//			getSupportFragmentManager().beginTransaction()
-//					.add(R.id.container, new PlaceholderFragment()).commit();
-//		}
+		SharedPreferences settings = getSharedPreferences("prefs", 0);
+	       String pressed = settings.getString("pressed", "0");
+	       
+	       if(pressed == "0")
+	       {
+		       radioGroup.check(R.id.radio0);
+
+	       }
+	       else if(pressed == "1")
+	       {
+		       radioGroup.check(R.id.radio1);
+
+	       }
+	       else if(pressed == "2")
+	       {
+		       radioGroup.check(R.id.radio2);
+
+	       }
+
 	}
+	
+	public void onClick (View view) {
+		  
+		  if (view.getId() == R.id.radio0) 
+		  {
+			  SharedPreferences settings = getSharedPreferences("prefs", 0);
+		      SharedPreferences.Editor editor = settings.edit();
+		      editor.putString("pressed", "0");
 
-//	@Override
-//	public boolean onCreateOptionsMenu(Menu menu) {
-//
-//		// Inflate the menu; this adtype filter textds items to the action bar if it is present.
-//		getMenuInflater().inflate(R.menu.settings, menu);
-//		return true;
-//	}
-//
-//	@Override
-//	public boolean onOptionsItemSelected(MenuItem item) {
-//		// Handle action bar item clicks here. The action bar will
-//		// automatically handle clicks on the Home/Up button, so long
-//		// as you specify a parent activity in AndroidManifest.xml.
-//		int id = item.getItemId();
-//		if (id == R.id.action_settings) {
-//			return true;
-//		}
-//		return super.onOptionsItemSelected(item);
-//	}
+		      editor.commit();
+		  }
+		  else if (view.getId() == R.id.radio1) 
+		  {
+			  SharedPreferences settings = getSharedPreferences("prefs", 0);
+		      SharedPreferences.Editor editor = settings.edit();
+		      editor.putString("pressed", "1");
+
+		      editor.commit();
+
+		  }
+		  else if (view.getId() == R.id.radio2) 
+		  {
+			  SharedPreferences settings = getSharedPreferences("prefs", 0);
+		      SharedPreferences.Editor editor = settings.edit();
+		      editor.putString("pressed", "2");
+
+		      editor.commit();
+
+		  }
+		  		  
+		} 
+
 
 }

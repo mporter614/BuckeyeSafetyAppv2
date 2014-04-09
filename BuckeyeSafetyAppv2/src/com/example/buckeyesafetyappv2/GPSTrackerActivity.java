a374f51dc5768855ac4955eed728cbb9eb44fba6
SolarBoyMatt <solarboymatt@gmail.com>
2014-04-08 21:52:43 -0400

    Draw circle around markers

--- BuckeyeSafetyAppv2/src/com/example/buckeyesafetyappv2/GPSTrackerActivity.java
+++ BuckeyeSafetyAppv2/src/com/example/buckeyesafetyappv2/GPSTrackerActivity.java
@@ -2,6 +2,7 @@ package com.example.buckeyesafetyappv2;
 
 import android.support.v4.app.Fragment;
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -10,12 +11,18 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
 import android.widget.Toast;
+import android.graphics.Color;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.Circle;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.location.Geofence;
+import com.google.android.gms.location.LocationClient;
+import com.google.android.gms.maps.model.CircleOptions;
+
 
 public class GPSTrackerActivity extends Activity {
 	
@@ -67,16 +74,59 @@ public class GPSTrackerActivity extends Activity {
     	double latitude2 = 40.013181;
     	double longitude2 = -83.013248;
     	
+    	int radius = 402;
+    	
+		SharedPreferences settings = getSharedPreferences("prefs", 0);
+	       String pressed = settings.getString("pressed", "0");
+	       
+	       if(pressed == "0")
+	       {
+		       radius = 402;
+
+	       }
+	       else if(pressed == "1")
+	       {
+		       radius = 804;
+
+	       }
+	       else if(pressed == "2")
+	       {
+		       radius = 1609;
+
+	       }
+    	
     	MarkerOptions marker1 = new MarkerOptions().position(new LatLng(latitude1, longitude1)).title("Test Crime 1");
     	MarkerOptions marker2 = new MarkerOptions().position(new LatLng(latitude1-.003000, longitude1-.002000)).title("Test Crime 2");
     	MarkerOptions marker3 = new MarkerOptions().position(new LatLng(latitude2, longitude2)).title("Test Crime 3");
     	
     	marker2.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN));
-    	marker3.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ORANGE));
+    	marker3.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_YELLOW));
     	
     	googleMap.addMarker(marker1);
     	googleMap.addMarker(marker2);
     	googleMap.addMarker(marker3);
+    	
+    	
+    	//Set up Circles
+    	CircleOptions circleOptions1 = new CircleOptions()
+    		.center(new LatLng(latitude1, longitude1)).radius(radius)
+    		.fillColor(0x40ff0000).strokeColor(0x90ff0000)
+    		.strokeWidth(5);
+    	CircleOptions circleOptions2 = new CircleOptions()
+    		.center(new LatLng(latitude1-.003000, longitude1-.002000)).radius(radius)
+    		.fillColor(0x40ff0000).strokeColor(0x90ff0000)
+    		.strokeWidth(5);
+    	CircleOptions circleOptions3 = new CircleOptions()
+    		.center(new LatLng(latitude2, longitude2)).radius(radius)
+    		.fillColor(0x40ff0000).strokeColor(0x90ff0000)
+    		.strokeWidth(5);
+
+    	//Add Circle to map
+    	googleMap.addCircle(circleOptions1);
+    	googleMap.addCircle(circleOptions2);
+    	googleMap.addCircle(circleOptions3);
+    	
+
     }
 	
 	@Override
